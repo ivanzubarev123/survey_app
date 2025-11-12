99672c0eb0b723119f66d9fc94b3963aedddb9e4
ivanzubarev123 <62092598+ivanzubarev123@users.noreply.github.com>
2025-11-12 19:32:25 +0300

    Update survey_app.py

--- survey_app.py
+++ survey_app.py
@@ -1,58 +1,50 @@
-# survey_app.py
-import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
-from psycopg2.extras import RealDictCursor
+from config import DB_CONFIG
+import os
 
 app = Flask(__name__)
 
-# Получаем URL базы из переменных окружения Render
-DB_URL = os.environ.get("DATABASE_URL")
-
 def get_conn():
-    return psycopg2.connect(DB_URL, cursor_factory=RealDictCursor)
+    return psycopg2.connect(**DB_CONFIG)
 
-@app.route("/")
+@app.route('/')
 def index():
-    # Список доступных опросов
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM opros WHERE dostup = TRUE;")
-            surveys = cur.fetchall()
-    return render_template("index.html", surveys=surveys)
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("SELECT id_opros, nazvanie, opisanie FROM opros ORDER BY data DESC;")
+        opros_list = cur.fetchall()
+    return render_template('index.html', opros_list=opros_list)
 
-@app.route("/opros/<int:id_opros>", methods=["GET", "POST"])
-def opros(id_opros):
-    if request.method == "POST":
-        answers = request.form  # словарь с ответами
-        with get_conn() as conn:
-            with conn.cursor() as cur:
-                # создаём сессию пользователя
-                cur.execute("INSERT INTO sessiya (id_opros) VALUES (%s) RETURNING id_sessii;", (id_opros,))
-                id_sessii = cur.fetchone()["id_sessii"]
-                # сохраняем ответы
-                for key, value in answers.items():
-                    cur.execute(
-                        "INSERT INTO otvet_polzovatelya (id_sessii, id_vopros, id_variant) VALUES (%s, %s, %s);",
-                        (id_sessii, key, value)
-                    )
-                conn.commit()
-        return redirect("/thanks")
-    else:
-        # Получаем вопросы и варианты ответов
-        with get_conn() as conn:
-            with conn.cursor() as cur:
-                cur.execute("SELECT * FROM vopros WHERE id_opros = %s ORDER BY poryadok;", (id_opros,))
-                questions = cur.fetchall()
-                for q in questions:
-                    cur.execute("SELECT * FROM variant_otveta WHERE id_vopros = %s;", (q["id_vopros"],))
-                    q["variants"] = cur.fetchall()
-        return render_template("opros.html", questions=questions, id_opros=id_opros)
+@app.route('/opros/<int:id_opros>')
+def show_opros(id_opros):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("SELECT tekst_vopros, id_vopros, tip_voprosa FROM vopros WHERE id_opros = %s ORDER BY poryadok;", (id_opros,))
+        voprosy = cur.fetchall()
+        voprosy_polnye = []
+        for tekst_vopros, id_vopros, tip in voprosy:
+            cur.execute("SELECT id_variant, tekst_otveta FROM variant_otveta WHERE id_vopros = %s;", (id_vopros,))
+            variants = cur.fetchall()
+            voprosy_polnye.append((id_vopros, tekst_vopros, tip, variants))
+    return render_template('opros.html', id_opros=id_opros, voprosy=voprosy_polnye)
 
-@app.route("/thanks")
+@app.route('/submit/<int:id_opros>', methods=['POST'])
+def submit(id_opros):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("INSERT INTO sessiya (id_opros) VALUES (%s) RETURNING id_sessii;", (id_opros,))
+        id_sessii = cur.fetchone()[0]
+
+        for key, value in request.form.items():
+            if key.startswith('q_'):
+                id_vopros = int(key.split('_')[1])
+                cur.execute(
+                    "INSERT INTO otvet_polzovatelya (id_sessii, id_vopros, id_variant) VALUES (%s, %s, %s)",
+                    (id_sessii, id_vopros, value)
+                )
+    return redirect(url_for('thanks'))
+
+@app.route('/thanks')
 def thanks():
-    return render_template("thanks.html")
+    return render_template('thanks.html')
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render назначает порт через переменную окружения
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
