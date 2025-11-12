be3310c6b7665a9141b68d98022a9c770cb291d2
ivanzubarev123 <62092598+ivanzubarev123@users.noreply.github.com>
2025-11-12 18:58:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DB_CONFIG = {
-    "host": "dpg-d3trfk49c44c73e8ptpg-a",
+    "host": "dpg-d3trfk49c44c73e8ptpg-a.oregon-postgres.render.com",
     "port": 5432,
-    "dbname": "survey",
+    "dbname": "survey_3ot3",
     "user": "survey_3ot3_user",
     "password": "7MQWoZO4wEfpX6yAqa6SY1BnL1jbfnmT"
 }
